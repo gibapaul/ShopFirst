84cffc89cb938acb6e118caeba0d9fd9059c1d43
gibapaul <bbgamer1088@gmail.com>
2024-09-30 14:23:40 +0700

    #3 Code role

--- server/config/dbconnect.js
+++ server/config/dbconnect.js
@@ -1,14 +1,15 @@
-const { default: mongoose} = require('mongoose')
-
+const { default: mongoose } = require('mongoose')
+mongoose.set('strictQuery', false)
 const dbConnect = async () => {
-    try{
+    try {
         const conn = await mongoose.connect(process.env.MONGODB_URI)
-        if(conn.connection.readyState === 1) console.log('DB connection is successfully')
-            else console.log('DB connecting');
+        if (conn.connection.readyState === 1) console.log('DB connection is successfully!')
+        else console.log('DB connecting');
+
     } catch (error) {
         console.log('DB connection is failed')
         throw new Error(error)
     }
-
 }
+
 module.exports = dbConnect
--- server/models/user.js
+++ server/models/user.js
@@ -1,39 +1,40 @@
 const mongoose = require('mongoose'); // Erase if already required
 const bcrypt = require('bcrypt')
+const crypto = require('crypto')
 // Declare the Schema of the Mongo model
 var userSchema = new mongoose.Schema({
-    firstname:{
-        type:String,
-        required:true,
+    firstname: {
+        type: String,
+        required: true,
     },
-    lastname:{
-        type:String,
-        require:true
+    lastname: {
+        type: String,
+        required: true,
     },
-    email:{
-        type:String,
-        required:true,
-        unique:true,
+    email: {
+        type: String,
+        required: true,
+        unique: true
     },
-    mobile:{
-        type:String,
-        required:true,
-        unique:true,
+    mobile: {
+        type: String,
+        required: true,
+        unique: true,
     },
-    password:{
-        type:String,
-        required:true,
+    password: {
+        type: String,
+        required: true,
     },
-    role:{
-        type:String,
-        default:'user',
+    role: {
+        type: String,
+        default: 'user',
     },
-    cart:{
+    cart: {
         type: Array,
         default: []
     },
-    address:[{type: mongoose.Types.ObjectId, ref: 'Address'}],
-    wishlist: [{type: mongoose.Types.ObjectId, ref: 'Product'}],
+    address: [{ type: mongoose.Types.ObjectId, ref: 'Address' }],
+    wishlist: [{ type: mongoose.Types.ObjectId, ref: 'Product' }],
     isBlocked: {
         type: Boolean,
         default: false
@@ -41,7 +42,7 @@ var userSchema = new mongoose.Schema({
     refreshToken: {
         type: String,
     },
-    passwordChangeAt: {
+    passwordChangedAt: {
         type: String
     },
     passwordResetToken: {
@@ -49,20 +50,29 @@ var userSchema = new mongoose.Schema({
     },
     passwordResetExpires: {
         type: String
-    },
-
-},{
+    }
+}, {
     timestamps: true
 });
-userSchema.pre('save', async function(next){
-    if (!this.isModified('password')) return next(); // Chỉ băm nếu mật khẩu đã thay đổi
-    const salt = await bcrypt.genSaltSync(10); // Sử dụng hàm bất đồng bộ
-    this.password = await bcrypt.hash(this.password, salt);
+
+userSchema.pre('save', async function (next) {
+    if (!this.isModified('password')) {
+        next()
+    }
+    const salt = bcrypt.genSaltSync(10)
+    this.password = await bcrypt.hash(this.password, salt)
 })
 userSchema.methods = {
     isCorrectPassword: async function (password) {
-        return await bcrypt.compare(password, this.password )
+        return await bcrypt.compare(password, this.password)
+    },
+    createPasswordChangedToken: function () {
+        const resetToken = crypto.randomBytes(32).toString('hex')
+        this.passwordResetToken = crypto.createHash('sha256').update(resetToken).digest('hex')
+        this.passwordResetExpires = Date.now() + 15 * 60 * 1000
+        return resetToken
     }
 }
+
 //Export the model
 module.exports = mongoose.model('User', userSchema);
